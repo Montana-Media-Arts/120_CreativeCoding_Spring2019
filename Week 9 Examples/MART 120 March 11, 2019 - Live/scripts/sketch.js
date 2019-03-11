e0eff1eba512699e9f1c46d6eddbdda1d4d19fd2
Michael Cassens <michaelcassens@gmail.com>
2019-03-11 11:04:15 -0600

    updated files

--- Week 9 Examples/MART 120 March 11, 2019 - Live/scripts/sketch.js
+++ Week 9 Examples/MART 120 March 11, 2019 - Live/scripts/sketch.js
@@ -1,6 +1,6 @@
 let img, enemy;
-var x = 0;
-var y = 0;
+var x = 200;
+var y = 200;
 var count = 0;
 var enemyX = 400;
 var enemyY = 500;
@@ -12,9 +12,53 @@ function setup() {
 function draw() {
    
     background(0);
-   
-        
+    fill(123,29,23);
+    if(keyIsPressed)
+    {
+        if(key == "a")
+        {
+            x-=10;
+        }
+        if(key == "d")
+        {
+            x+=10;
+        }
+        if(key == "w")
+        {
+            y-=10;
+        }
+    
+        if(key == "s")
+        {
+            y+=10;
+        }
+    }
+    rect(x,y,200,150);
+         
 }
+
+function keyPressed() {
+   /* if(key == "a")
+    {
+        print("in a");
+        x-=10;
+    }
+    else if(key == "d")
+    {
+        print("in d");
+        x+=10;
+    }
+    else if(key == "w")
+    {
+        y-=10;
+    }
+
+    else if(key == "s")
+    {
+        y+=10;
+    }
+    */
+  }
 
 
 
