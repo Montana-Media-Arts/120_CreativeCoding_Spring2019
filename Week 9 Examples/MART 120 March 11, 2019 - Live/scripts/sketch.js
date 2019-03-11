ee8ddaad6440de396baf53458b24d45400bb822a
Michael Cassens <michaelcassens@gmail.com>
2019-03-11 09:59:22 -0600

    added examples

---
+++ Week 9 Examples/MART 120 March 11, 2019 - Live/scripts/sketch.js
@@ -0,0 +1,21 @@
+let img, enemy;
+var x = 0;
+var y = 0;
+var count = 0;
+var enemyX = 400;
+var enemyY = 500;
+
+function setup() {
+    createCanvas(1000,800);
+}
+
+function draw() {
+   
+    background(0);
+   
+        
+}
+
+
+
+
